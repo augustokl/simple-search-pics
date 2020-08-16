1038f67a99cfa5bed6d4b921a40494a02a51c1b6
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-08-16 19:16:48 -0300

    image list adjust layout

--- src/components/ImageList/index.js
+++ src/components/ImageList/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './style.css';
+import ImageCard from '../ImageCard';
 
 const ImageList = ({images}) => {
   const imagesList = images.map(({id, urls, description})=> {
     return (
-      <img key={id} src={urls.regular} alt={description}/>
+      <ImageCard key={id} description={description} urls={urls}/>
     )
   })
 
-return <div>{imagesList}</div>
+return <div className="image-list">{imagesList}</div>
 }
 
 export default ImageList;
